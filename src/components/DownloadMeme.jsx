03dbbc44fd51a01d45ba2af9dc67fb5ecca6cdf9
uabdirasulm <ubbiniyazov04@gmail.com>
2023-07-03 11:39:36 +0500

    fix not downloading from input bug

--- src/components/DownloadMeme.jsx
+++ src/components/DownloadMeme.jsx
@@ -6,20 +6,22 @@ import "../scss/components/_download-meme.scss";
 
 const DownloadMeme = ({ memeImageRef }) => {
   const downloadMeme = () => {
-    html2canvas(memeImageRef.current).then((canvas) => {
-      // get the image data
-      const imageData = canvas.toDataURL("image/jpg");
-      // create a link element
-      const link = document.createElement("a");
-      link.href = imageData;
-      link.download = "meme.jpg";
-      // append the link to the document body
-      document.body.appendChild(link);
-      // click the link to download the file
-      link.click();
-      // remove the link from the document body
-      document.body.removeChild(link);
-    });
+    html2canvas(memeImageRef.current, { allowTaint: true, useCORS: true }).then(
+      (canvas) => {
+        // get the image data
+        const imageData = canvas.toDataURL("image/jpg");
+        // create a link element
+        const link = document.createElement("a");
+        link.href = imageData;
+        link.download = "meme.jpg";
+        // append the link to the document body
+        document.body.appendChild(link);
+        // click the link to download the file
+        link.click();
+        // remove the link from the document body
+        document.body.removeChild(link);
+      }
+    );
   };
   return (
     <div className="download-meme">
